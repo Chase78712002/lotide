c9fac2c19229cada55e3abf2554a96048b95b7c7
Alvin Lin <alvinch.lin@gmail.com>
2021-03-31 22:31:39 +0000

    Count everything in the allItems List and add them into the empty object variable called results

--- countOnly.js
+++ countOnly.js
@@ -8,9 +8,16 @@ const assertEqual = (actual, expected) => {
 
 const countOnly = (allItems, itemsToCount) => {
   // 1. figure out how many Jasons are in the allItems list
+  // 2. count everything in the allItems list
+    // 2a. add them to the empty object called results if not already in the object
+  
     let results = {};
   for (const item of allItems) {
-    console.log(item);
+    if (results[item]) {
+      results[item] += 1;
+    } else {
+      results[item] = 1;
+    }
   }
   return results;
 }
@@ -27,6 +34,7 @@ const firstNames = [
   "Joe"
 ];
 const result1 = countOnly(firstNames, {"Jason": true, "Karima": true, "Fang": true, "Agouhanna": false});
+console.log(result1);
 assertEqual(result1["Jason"], 1);
 assertEqual(result1["Karima"], undefined);
 assertEqual(result1["Fang"], 2);
