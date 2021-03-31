2beb329b70e0f29c7b2123cc333452acd30ca0c5
Alvin Lin <alvinch.lin@gmail.com>
2021-03-31 01:54:58 +0000

    created eqArrays function to compare arrays for assertEqual

---
+++ eqArrays.js
@@ -0,0 +1,33 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = (arr1, arr2) => {
+  let matchCount = 0;
+  for (let index in arr1) {
+    if (arr1[index] === arr2[index]) {
+      matchCount++;
+    }
+  }
+  if (matchCount === arr1.length) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// test code
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
