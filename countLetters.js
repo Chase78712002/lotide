99a0d7592548eff01be425b2934769a0567372bb
Alvin Lin <alvinch.lin@gmail.com>
2021-03-31 22:49:22 +0000

    copied the assert function, declared the function expression

---
+++ countLetters.js
@@ -0,0 +1,11 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const countLetters = (sentence) => {
+  // return a count for each letter in the sentence
+
+}
