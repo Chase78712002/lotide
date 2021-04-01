86fa68371acb037ee61185806b3a6457ee2ef686
Alvin Lin <alvinch.lin@gmail.com>
2021-04-01 03:32:24 +0000

    imported assertEqual function, created the foundation of the findKeyByValue function, test assertions created

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = (obj, val) => {
+  // scan obj
+  // return first key for the given val
+  // if no key found,
+    // return undefined
+}
+
+
+// test with assertEqual
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
