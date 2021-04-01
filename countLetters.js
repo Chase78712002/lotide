407eb1efb5f61b8ef56abeedfccfd173ba1ced26
Alvin Lin <alvinch.lin@gmail.com>
2021-04-01 02:18:56 +0000

    declared empty object, added each letter into the empty object as key and 1 count as value

--- countLetters.js
+++ countLetters.js
@@ -7,11 +7,18 @@ const assertEqual = (actual, expected) => {
 };
 const countLetters = (sentence) => {
   // return a count for each letter in the sentence
-    // the counts for each letter should return as an object with each letter as the key, and occurence as the value
+    // the counts for each letter should return as an *object* with each letter as the key, and occurence as the value
+  
   // 1. loop through the input and print out each letter
+  // 2. since output is an object, let's declare an empty object first
+  let output = {};
+  // 3. add each letter as a key in the output object
+      //3a. set each key's value as 1 first, return the output
+      
   for (const char of sentence) {
-    console.log(char);
+    output[char] = 1;
   }
+  return output;
 }
 
-console.log(countLetters('LHL'));
+console.log(countLetters('Lighthouse'));
