2a6db643628747279458e21d161c70988515aac3
Alvin Lin <alvinch.lin@gmail.com>
2021-04-01 02:10:01 +0000

    printed out each letter in the sentence

--- countLetters.js
+++ countLetters.js
@@ -8,5 +8,10 @@ const assertEqual = (actual, expected) => {
 const countLetters = (sentence) => {
   // return a count for each letter in the sentence
     // the counts for each letter should return as an object with each letter as the key, and occurence as the value
-  
+  // 1. loop through the input and print out each letter
+  for (const char of sentence) {
+    console.log(char);
+  }
 }
+
+console.log(countLetters('LHL'));
