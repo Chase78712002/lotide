6b9a553aeeb5fcf170f698b133d59175380f0aee
Alvin Lin <alvinch.lin@gmail.com>
2021-04-01 02:42:54 +0000

    defined the object structure to return

---
+++ letterPositions.js
@@ -0,0 +1,21 @@
+const letterPositions = (sentence) => {
+  const results = {};
+  // update results
+  return results;
+};
+
+// resulting obj should look like this ("lighthouse in the house") as arguement:
+/* 
+{
+  l:[0],
+  i:[1, 11],
+  g:[2],
+  h:[3, 5, 15, 18],
+  t:[4, 14],
+  o:[6, 19],
+  u:[7, 20],
+  s:[8, 21],
+  e:[9, 16, 22],
+  n:[12]
+}
+*/
