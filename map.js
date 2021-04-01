b197365861002d03108e9c17c7c8df21edb50734
Alvin Lin <alvinch.lin@gmail.com>
2021-04-01 22:59:08 +0000

    Defined the map function

---
+++ map.js
@@ -0,0 +1,7 @@
+const map = (array, callback) => {
+  // declare new array as empty
+  // loop through the array
+  // call the callback function
+    // push the result to the new array
+  // return new array
+};
