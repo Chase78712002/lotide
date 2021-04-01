32ca9268ea91d78370e1d954a06c27528fda3a7b
Alvin Lin <alvinch.lin@gmail.com>
2021-04-01 05:11:24 +0000

    added util library's inspect function

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,4 +1,6 @@
 // FUNCTION IMPLEMENTATION
 const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect; // <= add this line
+  
   // console.log appropriate message to the console
 };
