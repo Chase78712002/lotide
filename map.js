352ba9d3dc5b052ae57e063ca45a0db09cc586b3
Alvin Lin <alvinch.lin@gmail.com>
2021-04-02 00:17:37 +0000

    declared an empty array and return it

--- map.js
+++ map.js
@@ -1,7 +1,15 @@
 const map = (array, callback) => {
   // declare new array as empty
+  const newArray = [];
   // loop through the array
   // call the callback function
     // push the result to the new array
   // return new array
+  return newArray;
 };
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
