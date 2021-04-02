8f42d86cdd890509ab6b8945b19841b758d61edd
Alvin Lin <alvinch.lin@gmail.com>
2021-04-02 03:10:09 +0000

    completed the function, added 3 tests with assertEqual, all passed

--- findKey.js
+++ findKey.js
@@ -8,8 +8,40 @@ const assertEqual = (actual, expected) => {
 
 const findKey = (obj, callback) => {
   // scan the obj
-  // if callback is truthy
-    // return first key
+  for (let key in obj) {
+    // if callback is truthy
+    if(callback(obj[key])) {
+      // return first key
+      return key;
+    }
+  }
   // if no key found
     // return undefined
 }
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), 'noma');
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 4), undefined)
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3), 'Akaleri')
