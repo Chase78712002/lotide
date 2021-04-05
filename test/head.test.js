dc72bb5a31c6e82d7fad5f6c5adce25bdffc5b44
Alvin Lin <alvinch.lin@gmail.com>
2021-04-05 23:50:03 +0000

    added gitignore file to prevent pushing the entire node_modules folder to repo, installed mocha and chai, refactored head.test.js file to utilize the mocha framework and chai assertion library.

--- test/head.test.js
+++ test/head.test.js
@@ -1,6 +1,31 @@
+// const assertEqual = require('../assertEqual');
+const assert = require('chai').assert.strictEqual;
 const head = require('../head');
-const assertEqual = require('../assertEqual');
-// test code
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert(head([1, 2, 3]), 1);
+  });
+
+  it('returns "5" for ["5"]', () => {
+    assert(head(['5']), '5');
+  });
+
+  it('returns 5 for [5,6,7]', () => {
+    const actual = head([5,6,7]);
+    const expected = 5;
+    assert(actual,expected);
+  });
+
+  it('returns "Hello" for ["Hello", "Lighthouse", "Labs"]', () => {
+    const actual = head(["Hello", "Lighthouse", "Labs"]);
+    const expected = "Hello";
+    assert(actual, expected);
+  });
+
+  it('returns undefined for []', () => {
+    const actual = head([]);
+    const expected = undefined;
+    assert(actual, expected);
+  });
+});
