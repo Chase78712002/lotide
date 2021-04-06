c55773b2ad7ff7c87efbd2a616d4f74ee19de30d
Alvin Lin <alvinch.lin@gmail.com>
2021-04-06 00:19:48 +0000

    refactored middle.test.js and tail.test.js to that both tests utilize the mocha framework and chai assertion library

--- test/middle.test.js
+++ test/middle.test.js
@@ -1,9 +1,36 @@
-const assertArraysEqual = require('../assertArraysEqual') ;
+// const assertArraysEqual = require('../assertArraysEqual') ;
 const middle = require('../middle');
+const assert = require('chai').assert.deepEqual;
 
+describe("#middle", () => {
 
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2]), []);
+  it("returns [2] for [1, 2, 3]", () => {
+    const actual = middle([1, 2, 3]);
+    const expected = [2];
+    assert(actual, expected);
+  });
+
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    const actual = middle([1, 2, 3, 4, 5]);
+    const expected = [3];
+    assert(actual, expected);
+  });
+
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    const actual = middle([1, 2, 3, 4]);
+    const expected = [2, 3];
+    assert(actual, expected);
+  });
+
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    const actual = middle([1, 2, 3, 4, 5, 6]);
+    const expected = [3, 4];
+    assert(actual, expected);
+  });
+
+  it("returns [] for [1, 2,]", () => {
+    const actual = middle([1, 2]);
+    const expected = [];
+    assert(actual, expected);
+  });
+});
--- test/tail.test.js
+++ test/tail.test.js
@@ -1,6 +1,16 @@
-const assertEqual = require('../assertEqual');
 const tail = require('../tail');
+const assert = require('chai').assert;
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+describe("#tail", () => {
+  it('returns ["Lighthouse", "Labs"] for ["Yo Yo", "Lighthouse", "Labs"]', () => {
+    const actual = tail(["Yo Yo", "Lighthouse", "Labs"]);
+    const expected = ["Lighthouse", "Labs"];
+    assert.deepEqual(actual, expected);
+  });
+
+  it('returns [2,3,4,5] for [1,2,3,4,5]', () => {
+    const actual = tail([1,2,3,4,5]);
+    const expected = [2,3,4,5];
+    assert.deepEqual(actual, expected);
+  });
+});
